8dfff89d8b8a2cf48bc07cd2d641eae8ba9a6d57
Jakub Zárybnický <jakub@zarybnicky.com>
2022-01-08 00:18:49 +0100

    Simplify coordinate calculations

--- pyamaze/agent.py
+++ pyamaze/agent.py
@@ -54,7 +54,7 @@ class Agent:
         self.goal = goal if goal else self.parent_maze._goal
         self.parent_maze.agents.append(self)
         self._position = (x if x else parent_maze.rows, y if y else parent_maze.cols)
-        self.draw_agent()
+        self.coordinates = self.calculate_coords()
         self.draw_agent()
 
     @property
@@ -69,140 +69,109 @@ class Agent:
     def position(self):
         return self._position
 
-    @position.setter
-    def position(self, newpos):
-        self._position = newpos
-        self.draw_agent()
-
-    def draw_agent(self):
+    def calculate_coords(self):
         w = self.parent_maze.cell_width
         x = self.x * w - w + self.parent_maze.label_width
         y = self.y * w - w + self.parent_maze.label_width
-        fill = self.color.value[1]
-        if self.shape == "square":
-            if self.filled:
-                self.coordinates = (y, x, y + w, x + w)
-            else:
-                self.coordinates = (
-                    y + w / 2.5,
-                    x + w / 2.5,
-                    y + w / 2.5 + w / 4,
-                    x + w / 2.5 + w / 4,
-                )
+        if self.shape == "square" and self.filled:
+            return (y, x, y + w, x + w)
+        elif self.shape == "square":
+            return (y + w / 2.5, x + w / 2.5, y + w / 2.5 + w / 4, x + w / 2.5 + w / 4)
         else:
-            self.coordinates = (y + w / 2, x + 3 * w / 9, y + w / 2, x + 3 * w / 9 + w / 4)
+            return (y + w / 2, x + 3 * w / 9, y + w / 2, x + 3 * w / 9 + w / 4)
 
-        if self._head is not None:
-            if self.footprints is False:
-                self.parent_maze.canvas.delete(self._head)
-            else:
-                if self.shape == "square":
-                    self.parent_maze.canvas.itemconfig(self._head, fill=fill, outline="")
-                    self.parent_maze.canvas.tag_raise(self._head)
-                    try:
-                        self.parent_maze.canvas.tag_lower(self._head, "ov")
-                    except:
-                        pass
-                    if self.filled:
-                        lll = self.parent_maze.canvas.coords(self._head)
-                        oldcell = (
-                            round(((lll[1] - 26) / self.parent_maze.cell_width) + 1),
-                            round(((lll[0] - 26) / self.parent_maze.cell_width) + 1),
-                        )
-                        self.parent_maze.redraw_cell(oldcell)
-                else:
-                    self.parent_maze.canvas.itemconfig(self._head, fill=fill)
-                    self.parent_maze.canvas.tag_raise(self._head)
-                    try:
-                        self.parent_maze.canvas.tag_lower(self._head, "ov")
-                    except:
-                        pass
-                self._body.append(self._head)
-            if not self.filled or self.shape == "arrow":
-                if self.shape == "square":
-                    self.draw_square_head()
-                else:
-                    self.draw_arrow_head()
-                    o = self._orient % 4
-                    if o == 1:
-                        self._RCW()
-                        self._orient -= 1
-                    elif o == 3:
-                        self._RCCW()
-                        self._orient += 1
-                    elif o == 2:
-                        self._RCCW()
-                        self._RCCW()
-                        self._orient += 2
-            else:
-                self.draw_square_head()
+    @position.setter
+    def position(self, newpos):
+        self._position = newpos
+        self.coordinates = self.calculate_coords()
+        self.draw_agent()
+
+    def draw_agent(self):
+        if self._head is not None and not self.footprints:
+            self.parent_maze.canvas.delete(self._head)
+
+        if self._head is not None and self.footprints:
+            fill = self.color.value[1]
+            self.parent_maze.canvas.itemconfig(self._head, fill=fill)
+            self.parent_maze.canvas.tag_raise(self._head)
+            try:
+                self.parent_maze.canvas.tag_lower(self._head, "ov")
+            except:
+                pass
+            if self.shape == "square" and self.filled:
+                lll = self.parent_maze.canvas.coords(self._head)
+                oldcell = (
+                    round(((lll[1] - 26) / self.parent_maze.cell_width) + 1),
+                    round(((lll[0] - 26) / self.parent_maze.cell_width) + 1),
+                )
+                self.parent_maze.redraw_cell(oldcell)
+            self._body.append(self._head)
+
+        if self.shape == "square":
+            self._head = self.draw_square_head()
         else:
-            self.draw_square_head()
+            self._head = self.draw_arrow_head()
+            o = self._orient % 4
+            if o == 1:
+                self._RCW()
+                self._orient -= 1
+            elif o == 3:
+                self._RCCW()
+                self._orient += 1
+            elif o == 2:
+                self._RCCW()
+                self._RCCW()
+                self._orient += 2
 
     def draw_arrow_head(self):
         w = self.parent_maze.cell_width
-        fill = self.color.value[1]
-        self._head = self.parent_maze.canvas.create_line(
+        head = self.parent_maze.canvas.create_line(
             *self.coordinates,
-            fill=fill,
+            fill=self.color.value[0],
             arrow=FIRST,
-            arrowshape=(3 / 10 * w, 4 / 10 * w, 4 / 10 * w),
+            arrowshape=(3 / 10 * w, 4 / 10 * w, 4 / 10 * w)
         )
         try:
-            self.parent_maze.canvas.tag_lower(self._head, "ov")
+            self.parent_maze.canvas.tag_lower(head, "ov")
         except:
             pass
+        return head
 
     def draw_square_head(self):
-        fill = self.color.value[1]
-        self._head = self.parent_maze.canvas.create_rectangle(*self.coordinates, fill=fill, outline="")
+        fill = self.color.value[0]
+        head = self.parent_maze.canvas.create_rectangle(*self.coordinates, fill=fill, outline="")
         try:
-            self.parent_maze.canvas.tag_lower(self._head, "ov")
+            self.parent_maze.canvas.tag_lower(head, "ov")
         except:
             pass
         self.parent_maze.redraw_cell(self.position)
+        return head
 
     def _RCCW(self):
         """
         To Rotate the agent in Counter Clock Wise direction
         """
 
-        def pointNew(p, newOrigin):
-            return (p[0] - newOrigin[0], p[1] - newOrigin[1])
-
         w = self.parent_maze.cell_width
-        x = self.x * w - w + self.parent_maze.label_width
-        y = self.y * w - w + self.parent_maze.label_width
-        cent = (y + w / 2, x + w / 2)
-        p1 = pointNew((self.coordinates[0], self.coordinates[1]), cent)
-        p2 = pointNew((self.coordinates[2], self.coordinates[3]), cent)
-        p1CW = (p1[1], -p1[0])
-        p2CW = (p2[1], -p2[0])
-        p1 = p1CW[0] + cent[0], p1CW[1] + cent[1]
-        p2 = p2CW[0] + cent[0], p2CW[1] + cent[1]
-        self.coordinates = (*p1, *p2)
+        x = (self.x - .5) * w + self.parent_maze.label_width
+        y = (self.y - .5) * w + self.parent_maze.label_width
+        self.coordinates = (
+            self.coordinates[1] - x + y, x + y - self.coordinates[0],
+            self.coordinates[3] - x + y, x + y - self.coordinates[2]
+        )
         self.parent_maze.canvas.coords(self._head, *self.coordinates)
         self._orient = (self._orient - 1) % 4
 
     def _RCW(self):
-        """
-        To Rotate the agent in Clock Wise direction
-        """
-
-        def pointNew(p, newOrigin):
-            return (p[0] - newOrigin[0], p[1] - newOrigin[1])
+        """Rotate the agent in Clock Wise direction"""
 
         w = self.parent_maze.cell_width
-        x = self.x * w - w + self.parent_maze.label_width
-        y = self.y * w - w + self.parent_maze.label_width
-        cent = (y + w / 2, x + w / 2)
-        p1 = pointNew((self.coordinates[0], self.coordinates[1]), cent)
-        p2 = pointNew((self.coordinates[2], self.coordinates[3]), cent)
-        p1CW = (-p1[1], p1[0])
-        p2CW = (-p2[1], p2[0])
-        p1 = p1CW[0] + cent[0], p1CW[1] + cent[1]
-        p2 = p2CW[0] + cent[0], p2CW[1] + cent[1]
-        self.coordinates = (*p1, *p2)
+        x = (self.x - .5) * w + self.parent_maze.label_width
+        y = (self.y - .5) * w + self.parent_maze.label_width
+        self.coordinates = (
+            x + y - self.coordinates[1], x - y + self.coordinates[0],
+            x + y - self.coordinates[3], x - y + self.coordinates[2]
+        )
         self.parent_maze.canvas.coords(self._head, *self.coordinates)
         self._orient = (self._orient + 1) % 4
 
